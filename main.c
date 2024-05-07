b49a85731611e3e3a961434d28b371cb55502764
Gatoll <107396045+Gatoll@users.noreply.github.com>
2024-05-07 19:26:18 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <fcntl.h>
+int main(void)
+{
+	int fd;
+	char *line;
+
+	fd = open("test.txt", O_RDONLY);
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
---
+++ main_bonus.c
@@ -0,0 +1,34 @@
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	main(void)
+{
+	char	*line;
+
+	int fd, fd2, fd3;
+	fd = open("test.txt", O_RDONLY);
+	fd2 = open("test2.txt", O_RDONLY);
+	fd3 = open("test3.txt", O_RDONLY);
+	if (fd < 0 || fd2 < 0 || fd3 < 0)
+	{
+		perror("open");
+		return (1);
+	}
+	while ((line = get_next_line(fd)) || (line = get_next_line(fd2))
+		|| (line = get_next_line(fd3)))
+	{
+		if (line)
+		{
+			printf("%s", line);
+			free(line);
+		}
+	}
+	close(fd);
+	close(fd2);
+	close(fd3);
+	return (0);
+}
